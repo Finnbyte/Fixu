f4900ee09e007b508698fa6565f0e89ba13f2942
Finnbyte <eelpy@protonmail.com>
2023-11-29 21:45:47 +0200

    Make cuidId generation happen by constant defining length

--- backend/db/common.ts
+++ backend/db/common.ts
@@ -1,6 +1,8 @@
 import { createId } from "@paralleldrive/cuid2";
 import { varchar } from "drizzle-orm/mysql-core";
 
+export const CUID_LENGTH = 64;
+
 export function cuidId() {
-  return varchar("id", { length: 128 }).$defaultFn(() => createId());
+  return varchar("id", { length: CUID_LENGTH }).$defaultFn(() => createId());
 }
