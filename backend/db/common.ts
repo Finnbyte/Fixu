0c70339850fad1f4a253cb1291b3725bb8aa6ccf
Finnbyte <eelpy@protonmail.com>
2023-11-30 00:28:34 +0200

    Add zod schema for cuid

--- backend/db/common.ts
+++ backend/db/common.ts
@@ -1,8 +1,11 @@
 import { createId } from "@paralleldrive/cuid2";
 import { varchar } from "drizzle-orm/mysql-core";
+import { z } from "zod";
 
-export const CUID_LENGTH = 64;
+export const CUID_LENGTH = 32;
 
 export function cuidId() {
   return varchar("id", { length: CUID_LENGTH }).$defaultFn(() => createId());
 }
+
+export const cuidSchema = z.string().max(CUID_LENGTH);
