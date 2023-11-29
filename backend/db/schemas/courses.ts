a68d86fc007510969e9027ef91bf5e4a516ac817
Finnbyte <eelpy@protonmail.com>
2023-11-30 00:28:10 +0200

    Add courses schema and junction between courses and users

---
+++ backend/db/schemas/courses.ts
@@ -0,0 +1,25 @@
+import { boolean, date, mysqlTable, text, varchar } from "drizzle-orm/mysql-core";
+import { CUID_LENGTH, cuidId } from "../common";
+import { users } from "./users";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
+
+export const courses = mysqlTable("courses", {
+  id: cuidId(),
+  name: varchar("name", { length: 62 }).unique().notNull(),
+  description: text("description").notNull(),
+  createdAt: date("created_at").notNull(),
+  endedAt: date("ended_at")
+});
+
+export const usersCourses = mysqlTable("users_courses", {
+  id: cuidId(),
+  userId: varchar("user_id", { length: CUID_LENGTH }).references(() => users.id),
+  courseId: varchar("course_id", { length: CUID_LENGTH }).references(() => courses.id),
+  isTeaching: boolean("is_teaching")
+});
+
+export const selectCourseSchema = createSelectSchema(courses);
+export const insertCourseSchema = createInsertSchema(courses);
+
+export type Course = z.infer<typeof selectCourseSchema>;
