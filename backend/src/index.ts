e3b0d84b91b8873cfe52855db9bb4695ee434716
Finnbyte <eelpy@protonmail.com>
2023-11-29 21:47:32 +0200

    Change server to Fastify
    TRPC was not a good fit for this since it's a school project.
    Fastify with RESTful API design makes me fill the project criteria.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,62 @@
-import { createHTTPServer } from '@trpc/server/adapters/standalone';
-import { z } from "zod"
-import { publicProcedure, router } from './trpc';
-import { TRPCError } from '@trpc/server';
+import Fastify from "fastify";
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql, { Connection } from "mysql2";
+import sessionRoute from "./api/modules/session/session.route";
+import usersRoute from "./api/modules/users/users.route";
+import { sessionSchemas } from "./api/modules/session/session.schema";
+import { usersSchemas } from "./api/modules/users/users.schema";
+import { uptime } from "./utils/uptime";
+import assert from "node:assert";
 
-const appRouter = router({
-  getUsers: publicProcedure.query(async () => {
-    return ["user1", "user2"];
-  }),
-  fails: publicProcedure.query(async () => {
-    throw new TRPCError({
-        code: "BAD_REQUEST"
-    })
-  })
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv").config();
+
+function initializeDatabase(): Connection {
+  try {
+    const connection = mysql.createConnection({
+      host: "localhost",
+      user: process.env.USERNAME ?? "root",
+      database: process.env.DATABASE ?? "fixu"
+    });
+    return connection;
+  } catch (err) {
+    console.error("Failed to establish connection with database. Reason:", err);
+    process.exit(1);
+  }
+}
+
+export const db = drizzle(initializeDatabase());
+
+const server = Fastify({
+  logger: true,
 });
 
-const server = createHTTPServer({
-  router: appRouter,
+let startedAt: Date;
+
+server.get("/healthcheck", () => {
+  const now = new Date();
+  assert(startedAt !== undefined, "ERROR: startedAt is undefined");
+  return {
+    msg: "I am alive.",
+    uptime: uptime(startedAt, now)
+  };
 });
- 
-server.listen(3000);
- 
-// Export type router type signature,
-// NOT the router itself.
-export type AppRouter = typeof appRouter;
+
+server.decorateRequest("user", null);
+
+for (const schema of [...sessionSchemas, ...usersSchemas]) {
+  server.addSchema(schema);
+}
+
+server.register(usersRoute, { prefix: "api/users" });
+server.register(sessionRoute, { prefix: "api/session" });
+
+server.listen({ port: 8080 }, (err) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  console.log("Server started succesfully");
+  startedAt = new Date();
+});
