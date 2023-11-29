24cb92e43d7c96ae7bc62f090e48c83d3776e54e
Finnbyte <eelpy@protonmail.com>
2023-11-29 22:49:06 +0200

    Create registration schema to validate /users POST

--- backend/src/api/modules/users/users.schema.ts
+++ backend/src/api/modules/users/users.schema.ts
@@ -3,13 +3,26 @@ import { insertUserSchema, selectUserSchema } from "../../../../db/schemas/users
 import { z } from "zod";
 import { CUID_LENGTH } from "../../../../db/common";
 
+/**
+ * 8-length and at least one uppercase, lowercase, digit and special character.
+ */
+const strongPasswordRegex = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/;
+
 const userIdSchema = z.string().max(CUID_LENGTH);
+
+const registerSchema = insertUserSchema
+  .omit({ id: true, privilege: true })
+  .merge(
+    z.object({
+      email: z.string().max(78).email(),
+      password: z.string().regex(strongPasswordRegex),
+    })
+  );
 
 export type CreateUserInput = z.infer<typeof insertUserSchema>;
 export type User = z.infer<typeof selectUserSchema>;
 
 export const { schemas: usersSchemas, $ref } = buildJsonSchemas({
-  insertUserSchema,
-  selectUserSchema,
+  registerSchema,
   userIdSchema
 }, { $id: "usersSchemas" });
