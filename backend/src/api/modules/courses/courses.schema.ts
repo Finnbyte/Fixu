0a0d8f7c54707b2ba1bc4887cd35ac44d133d7b7
Finnbyte <eelpy@protonmail.com>
2023-11-30 21:52:30 +0200

    Add Zod schemas for creating courses

--- backend/src/api/modules/courses/courses.schema.ts
+++ backend/src/api/modules/courses/courses.schema.ts
@@ -1,7 +1,16 @@
 import { z } from "zod";
+import { insertCourseSchema } from "../../../../db/schemas/courses";
+import { buildJsonSchemas } from "fastify-zod";
 
 export const courseQueryStringSchema = z.object({
   name: z.string()
 });
 
+export const createCourseSchema = insertCourseSchema.omit({ id: true, createdAt: true, endedAt: true });
+
+export const { schemas: coursesSchemas, $ref } = buildJsonSchemas({
+  createCourseSchema
+});
+
 export type CourseQueryString = z.infer<typeof courseQueryStringSchema>
+export type CreateCourseInput = z.infer<typeof createCourseSchema>
