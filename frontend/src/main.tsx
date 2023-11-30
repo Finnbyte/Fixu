215bd1088c4d933c83591abff21b0b9cca88f1b6
Finnbyte <eelpy@protonmail.com>
2023-12-01 01:05:20 +0200

    Add base for Fixu frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Login from './routes/login/Login.tsx';
+import Register from './routes/register/Register.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/sign-up",
+    element: <Register />
+  }
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
