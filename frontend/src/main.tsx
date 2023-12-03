6ad4ec24bb9c0956749b31746094313a225d64d8
Finnbyte <eelpy@protonmail.com>
2023-12-03 03:06:28 +0200

    Add routes array

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,25 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Login from './routes/login/Login.tsx';
-import Register from './routes/register/Register.tsx';
+import { RouteObject, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Login from './pages/login/Login.tsx';
+import Register from './pages/register/Register.tsx';
+import AuthenticatedView from './components/AuthenticatedView/AuthenticatedView.tsx';
+import Root from './pages/root/Root.tsx';
+import Courses from './pages/courses/Courses.tsx';
+import Home from './pages/home/Home.tsx';
+import "./index.css";
 
-const router = createBrowserRouter([
+export const ROUTES: RouteObject[] = [
   {
     path: "/",
-    element: <App />,
+    element: <Root />,
+  },
+  {
+    path: "/app",
+    element: <AuthenticatedView />,
+    children: [
+      {
+        element: <Home />,
+        index: true,
+      },
+      {
+        path: "courses",
+        element: <Courses />,
+      },
+    ],
   },
   {
     path: "/login",
-    element: <Login />
+    element: <Login />,
   },
   {
     path: "/sign-up",
-    element: <Register />
-  }
+    element: <Register />,
+  },
+];
 
-]);
+const router = createBrowserRouter(ROUTES);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
