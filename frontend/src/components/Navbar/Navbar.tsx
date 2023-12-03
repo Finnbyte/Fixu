b672901a8ff323a558632b637781daf406f03a79
Finnbyte <eelpy@protonmail.com>
2023-12-04 00:56:19 +0200

    Make Navbar use user store to get user's data to show

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 import { NavLink, NavLinkProps } from "react-router-dom";
 import styles from "./Navbar.module.scss";
 import { CalendarMonth, Home, LibraryBooks, Logout, Settings } from "@mui/icons-material";
+import { User } from "../../../../backend/db/schemas/users";
+import { useUserStore } from "../../store/user";
 
 function CustomNavLink(props: NavLinkProps) {
   return (
@@ -12,6 +14,16 @@ function CustomNavLink(props: NavLinkProps) {
 }
 
 export default function Navbar() {
+  const { firstName, lastName, email } = useUserStore((state) => ({
+    firstName: state.data?.firstName,
+    lastName: state.data?.lastName,
+    email: state.data?.email,
+  }));
+
+  const handleLogout = async () => {
+    await fetch("/api/session", { method: "DELETE", credentials: "include" });
+  };
+
   return (
     <nav id={styles.navbar}>
       <CustomNavLink to={"/app"} end>
@@ -33,10 +45,12 @@ export default function Navbar() {
 
       <div className={styles.profile}>
         <div>
-          <p>Jarmo Jortikka</p>
-          <p id={styles.email}>jarmo.jortikka@edu.turku.fi</p>
+          <p>
+            {firstName} {lastName}
+          </p>
+          <p id={styles.email}>{email}</p>
         </div>
-        <div onClick={() => console.log("clicked")}>
+        <div onClick={handleLogout}>
           <Logout className={styles["logout-button"]} />
         </div>
       </div>
