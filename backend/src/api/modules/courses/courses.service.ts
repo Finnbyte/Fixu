8ccd9129dba6d455807fa7d2068510bab65a29d4
Finnbyte <eelpy@protonmail.com>
2024-01-13 14:35:03 +0200

    Add services to add course user and list course users

--- backend/src/api/modules/courses/courses.service.ts
+++ backend/src/api/modules/courses/courses.service.ts
@@ -1,7 +1,8 @@
-import { eq } from "drizzle-orm";
+import { eq, and } from "drizzle-orm";
 import { db } from "../../..";
-import { courses } from "../../../../db/schemas/courses";
+import { courses, usersCourses } from "../../../../db/schemas/courses";
 import { CreateCourseFull } from "./courses.schema";
+import { users } from "../../../../db/schemas/users";
 
 export async function fetchAllCourses() {
   return await db.select().from(courses);
@@ -21,6 +22,27 @@ export async function fetchCourseById(courseId: string) {
   });
 
   return course;
+}
+
+export async function fetchCourseUsers(courseId: string, filterPrivilege?: "student" | "teacher") {
+  const showOnlyTeachers = filterPrivilege === "teacher";
+
+  const result = await db
+    .select()
+    .from(usersCourses)
+    .where(
+      and(
+        eq(usersCourses.courseId, courseId),
+        filterPrivilege && eq(usersCourses.isTeaching, showOnlyTeachers)
+      )
+    )
+    .leftJoin(users, eq(users.id, usersCourses.userId));
+
+  return result.map((element) => element.users);
+}
+
+export async function createCourseUser(courseId: string, userId: string, isTeaching: boolean) {
+  await db.insert(usersCourses).values({ courseId, userId, isTeaching });
 }
 
 export async function createCourse(course: CreateCourseFull) {
