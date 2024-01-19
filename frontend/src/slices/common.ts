f001463d184e90ba1d65d9e0712b5460be3bea6b
Finnbyte <eelpy@protonmail.com>
2024-01-19 13:38:43 +0200

    Add clean common utility types and functions

--- frontend/src/slices/common.ts
+++ frontend/src/slices/common.ts
@@ -1,2 +1,15 @@
+import { SerializedError } from "@reduxjs/toolkit";
 
 export type ActionStatus = "idle" | "loading" | "succeeded" | "failed";
+
+export function createInitialState<T>(data: T) {
+  return {
+    data,
+    status: "idle",
+    error: null,
+  } as {
+    data: T;
+    status: ActionStatus;
+    error: SerializedError | null;
+  };
+}
