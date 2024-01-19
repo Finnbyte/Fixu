a48e9382a934f2ce604dab1757c000e9140c7dbf
Finnbyte <eelpy@protonmail.com>
2024-01-19 13:38:44 +0200

    Fix API not being defined in store

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userSlice } from "./slices/user";
 import { calendarSlice } from "./slices/calendar";
+import { apiSlice } from "./slices/api";
 
 export const store = configureStore({
   reducer: {
     user: userSlice.reducer,
-    calendar: calendarSlice.reducer
-  }
+    calendar: calendarSlice.reducer,
+    api: apiSlice.reducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>
