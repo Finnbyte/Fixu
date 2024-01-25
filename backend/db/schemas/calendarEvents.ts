b1a2d5d22ae9aa6df5473388587ba20a08e6a999
Finnbyte <eelpy@protonmail.com>
2024-01-25 11:16:43 +0200

    Change calendar event schema to take course/personal into consideration

--- backend/db/schemas/calendarEvents.ts
+++ backend/db/schemas/calendarEvents.ts
@@ -1,12 +1,17 @@
-import { date, mysqlTable, varchar } from "drizzle-orm/mysql-core";
+import { date, mysqlEnum, mysqlTable, varchar } from "drizzle-orm/mysql-core";
 import { cuidId, primaryCuidIdKey } from "../common";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
 import { users } from "./users";
+import { courses } from "./courses";
+
+const calendarEventTypes = ["personal", "course"] as const;
 
 export const calendarEvents = mysqlTable("calendar_events", {
   id: primaryCuidIdKey(),
-  attendee: cuidId("attendee_id").references(() => users.id),
+  type: mysqlEnum("type", calendarEventTypes).notNull(),
+  courseId: cuidId("course_id").references(() => courses.id),
+  author_id: cuidId("author_id").references(() => users.id),
   title: varchar("title", { length: 40 }).notNull(),
   date: date("date", { mode: "string" }).notNull(),
 });
@@ -15,3 +20,4 @@ export const selectCalendarEventSchema = createSelectSchema(calendarEvents);
 export const insertCalendarEventSchema = createInsertSchema(calendarEvents);
 
 export type CalendarEvent = z.infer<typeof selectCalendarEventSchema>;
+export type CalendarEventType = typeof calendarEventTypes[keyof typeof calendarEventTypes];
