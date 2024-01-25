36918e44770d479eb86222bed5969ae4e5bd6aef
Finnbyte <eelpy@protonmail.com>
2024-01-25 14:52:00 +0200

    Add working query for course-related and personal calendar events

--- backend/src/api/modules/calendar-events/calendarEvents.service.ts
+++ backend/src/api/modules/calendar-events/calendarEvents.service.ts
@@ -2,12 +2,22 @@ import { eq } from "drizzle-orm";
 import { db } from "../../..";
 import { calendarEvents } from "../../../../db/schemas/calendarEvents";
 import { CreateCalendarEvent } from "./calendarEvents.schema";
+import { usersCourses } from "../../../../db/schemas/courses";
 
 export async function fetchCalendarEvents(userId: string) {
-  return await db
+  const rows = await db
     .select()
     .from(calendarEvents)
-    .where(eq(calendarEvents.attendee, userId));
+    .leftJoin(usersCourses, eq(usersCourses.courseId, calendarEvents.courseId));
+
+  return rows
+    .filter((row) =>
+      row.calendar_events.type === "personal" &&
+      row.calendar_events.authorId !== userId
+        ? false
+        : true
+    )
+    .map((row) => row.calendar_events);
 }
 
 export async function createCalendarEvent(calendarEvent: CreateCalendarEvent) {
