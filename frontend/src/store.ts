63672b44ae2d1885bcf5c09c3fc3c2ccdfa27109
Finnbyte <eelpy@protonmail.com>
2024-01-26 14:07:51 +0200

    Add persistance of calendar's selected date

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { userSlice } from "./slices/user";
 import { calendarSlice } from "./slices/calendar";
 import { apiSlice } from "./slices/api";
+import { addMinutes } from "date-fns";
 
 export const store = configureStore({
   reducer: {
@@ -10,6 +11,17 @@ export const store = configureStore({
     api: apiSlice.reducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+});
+
+store.subscribe(() => {
+  // Persist selected date
+  localStorage.setItem(
+    "calendarSelectedDate",
+    JSON.stringify({
+      date: store.getState().calendar.data.selectedDate,
+      invalidate: addMinutes(new Date(), 30),
+    })
+  );
 });
 
 export type RootState = ReturnType<typeof store.getState>
