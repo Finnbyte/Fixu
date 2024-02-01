0efc217776b607334d731ba3f58139cade8725d1
Finnbyte <eelpy@protonmail.com>
2024-02-01 12:58:17 +0200

    Add schema to create proper routes for updating/adding/deleting calendar events

--- backend/src/api/modules/calendar-events/calendarEvents.schema.ts
+++ backend/src/api/modules/calendar-events/calendarEvents.schema.ts
@@ -1,13 +1,20 @@
 import { z } from "zod";
 import { insertCalendarEventSchema } from "../../../../db/schemas/calendarEvents";
 import { buildJsonSchemas } from "fastify-zod";
+import { cuidSchema } from "../../../../db/common";
 
 export const createCalendarEventSchema = insertCalendarEventSchema.omit({ id: true });
 export const createCalendarEventBodySchema = insertCalendarEventSchema.omit({ id: true, attendee: true });
 
+export const updateCalendarEventSchema = z.object
+
 export const calendarEventQuery = z.object({
   year: z.number().optional(),
   month: z.number().optional(),
+})
+
+export const calendarEventParams = z.object({
+  calendarEventId: cuidSchema
 })
 
 export type CreateCalendarEvent = z.infer<typeof createCalendarEventSchema>
@@ -15,6 +22,7 @@ export type CreateCalendarEventBody = z.infer<typeof createCalendarEventBodySche
 
 export const { schemas: calendarEventsSchemas, $ref } = buildJsonSchemas({
   calendarEventQuery,
+  calendarEventParams,
   createCalendarEventSchema,
   createCalendarEventBodySchema
 }, { $id: "calendarEventsSchemas" });
