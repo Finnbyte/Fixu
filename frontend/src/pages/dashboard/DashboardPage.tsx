3f2d891e743f850750cfa7dc3ca66af5218bd6e9
Finnbyte <eelpy@protonmail.com>
2024-02-08 01:02:40 +0200

    Add padding to dashboard

--- frontend/src/pages/dashboard/DashboardPage.tsx
+++ frontend/src/pages/dashboard/DashboardPage.tsx
@@ -1,6 +1,20 @@
+import { Course } from "../../../../backend/db/schemas/courses";
+import styles from "./DashboardPage.module.scss";
+
+function getRecentlyViewedCourses(): Course[] | null {
+  const result = localStorage.getItem("recentlyViewedCourses");
+  if (!result) return null;
+  try {
+    const courses = JSON.parse(result) as Course[];
+    return courses.slice(0, 2);
+  } catch {
+    return null
+  }
+}
+
 export default function Dashboard() {
   return (
-    <div>
+    <div className={styles.page}>
       <h1>My recently viewed courses</h1>
     </div>
   )
