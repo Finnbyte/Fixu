cc4978205fdacb4b324e811be0f8b14883fea014
Finnbyte <eelpy@protonmail.com>
2024-03-06 14:56:15 +0200

    Rewrite AuthGuard to use authService

--- frontend/src/components/AuthGuard/AuthGuard.tsx
+++ frontend/src/components/AuthGuard/AuthGuard.tsx
@@ -1,6 +1,7 @@
 import { Navigate, useLocation } from "react-router-dom";
 import AuthenticatedView from "../AuthenticatedView/AuthenticatedView";
-import { useGetUserDataQuery } from "../../slices/api";
+import { useEffect, useState } from "react";
+import { authService } from "../../auth/authService";
 
 function createRedirectURL(pathname: string) {
   let URL = "/login";
@@ -11,19 +12,30 @@ function createRedirectURL(pathname: string) {
 }
 
 export function AuthGuard() {
-  const { data, error } = useGetUserDataQuery();
+  const [userId, setUserId] = useState<string | null | undefined>(undefined);
   const location = useLocation();
 
-  if (!data && !error) {
-    return null;
+  useEffect(() => {
+    authService.ensureAuth().then(res => {
+      if (!res.ok) {
+        setUserId(null);
+        return;
+      }
+
+      res.json().then(data => {
+        setUserId(data["userId"]);
+      });
+    });
+  }, []);
+
+  if (userId === undefined) {
+    return <></>;
   }
 
-  if (!data && error) {
-    console.log(error);
+  if (userId === null) {
     const redirectionURL = createRedirectURL(location.pathname);
-
     return <Navigate to={redirectionURL} replace />;
   }
 
-  return <AuthenticatedView userId={data!.userId} />;
+  return <AuthenticatedView userId={userId} />;
 }
